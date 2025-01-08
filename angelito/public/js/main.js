a317f465b1f29b74af42310543fce42943b383d1
Angel Eugenio Charca Blanco <ablanco837@gmail.com>
2025-01-08 11:23:15 -0400

    changing git file

---
+++ angelito/public/js/main.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+  new WOW().init();
+
+  $("#status").fadeOut();
+    $("#preloader").delay(450).fadeOut("slow");
+
+    $(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+      $('.Web-Header').addClass('Sticky');
+    }
+    if ($(window).scrollTop() < 50) {
+      $('.Web-Header').removeClass('Sticky');
+    }
+  });
+
+  var sideMenu = $(".side-panel");
+  var panelOverlay = $(".panel-overlay");
+  var popupSearch = $(".popup-search");
+
+  $("#openSideMenu").click(function(event) {
+      event.preventDefault();
+      if (sideMenu.hasClass('side-panel-open')) {
+          sideMenu.removeClass('side-panel-open');
+          panelOverlay.removeClass('active');
+          // $(this).removeClass('active');
+      } else {
+          sideMenu.addClass('side-panel-open');
+          panelOverlay.addClass('active');
+          // $(this).addClass('active');
+      }
+  });
+  $("#closeSideMenu").click(function(event) {
+      event.preventDefault();      
+      if (sideMenu.hasClass('side-panel-open')) {
+          sideMenu.removeClass('side-panel-open');
+          panelOverlay.removeClass('active');
+          // $(this).removeClass('active');
+      } else {
+          sideMenu.addClass('side-panel-open');
+          panelOverlay.addClass('active');
+          // $(this).addClass('active');
+      }
+  });
+  $("#openPopupSearch").click(function(event) {
+      event.preventDefault();
+      if (popupSearch.hasClass('show')) {
+          popupSearch.removeClass('show');
+          $(this).removeClass('ic-close');
+      } else {
+          popupSearch.addClass('show');
+          $(this).addClass('ic-close');
+      }
+  });
+
+  // Animacion de Scroll
+  let go = $('.desplazar');
+
+  go.click(function(e) {
+    e.preventDefault();
+
+    const target = $(this.hash);
+    const offsetTop = target.offset().top;
+    const duration = 0;
+
+    $('body, html').animate(
+      {
+        scrollTop: offsetTop,
+      },
+
+      duration
+    );
+
+  });
+  // Animacion de Scroll END
+
+  var myCarousel = document.querySelector('#carouselHero')
+  var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 4000,
+      pause: false,
+      touch: true,
+      keyboard: true,
+      // wrap: false,
+      // ride: false
+  })
+
+}); 
